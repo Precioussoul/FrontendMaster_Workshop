da84a7bface6a942891cc3a7dfd83f2e333bff43
Precioussoul <habsoff@gmail.com>
2023-10-15 09:52:49 +0100

    feat: added Menu.js

--- you_might_not_need_framework/app.js
+++ you_might_not_need_framework/app.js
@@ -1,12 +1,11 @@
-window.addEventListener(
-    "DOMContentLoaded",
-    () => {
-        let nav = document.createElement("nav")
-        console.log("====================================")
-        console.log(nav)
-        console.log("====================================")
-    }, {
-        once: true,
-        passive: true,
-    }
-)
+import Store from "./services/Store.js"
+import API from "./services/API.js"
+import { loadData } from "./services/Menu.js"
+
+window.app = {}
+app.store = Store
+
+// its better to wait for the DOM  before manipulation
+window.addEventListener("DOMContentLoaded", () => {
+    loadData()
+})
