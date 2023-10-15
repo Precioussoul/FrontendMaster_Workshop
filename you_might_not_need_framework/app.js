3f9e2f2f39cde082c6aa685afc3b5f73a18228f1
Precioussoul <habsoff@gmail.com>
2023-10-15 09:43:06 +0100

    added API.js and Store.js to  services folder

---
+++ you_might_not_need_framework/app.js
@@ -0,0 +1,12 @@
+window.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        let nav = document.createElement("nav")
+        console.log("====================================")
+        console.log(nav)
+        console.log("====================================")
+    }, {
+        once: true,
+        passive: true,
+    }
+)
