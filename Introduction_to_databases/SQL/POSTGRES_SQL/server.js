fa3ae09e5e5dd827240bfbafadde9fbb19757a01
Precioussoul <habsoff@gmail.com>
2023-10-24 22:48:41 +0100

    feat: update server.js file

--- Introduction_to_databases/SQL/POSTGRES_SQL/server.js
+++ Introduction_to_databases/SQL/POSTGRES_SQL/server.js
@@ -1,31 +1,38 @@
 const express = require("express")
 const { Pool } = require("pg")
-    // for the purpose of this course
 const pool = new Pool({
-    connectionString: "postgres://postgres:mysecretpassword@localhost:5432/message_boards",
+    connectionString: "postgresql://postgres:mysecretpassword@localhost:5432/message_boards",
 })
 
 async function init() {
     const app = express()
 
-    app.get("/", async(req, res) => {
+    app.get("/get", async(req, res) => {
         const client = await pool.connect()
-
-        const [commentRes, boardRes] = await Promise.all([
+        const [commentsRes, boardRes] = await Promise.all([
             client.query(
-                "SELECT * FROM comments NATURAL LEFT JOIN  rich_content WHERE board_id =$1", [req.query.search]
+                `SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = ${req.query.search}`
+                // "SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = $1",
+                // [req.query.search]
             ),
-            client.query("SELECT * FROM boards WHERE board_id =$1", [
+            client.query("SELECT * FROM boards WHERE board_id = $1", [
                 req.query.search,
             ]),
         ])
-
-        res.json({
-            status: "ok",
-            board: boardRes.rows[0] || {},
-            post: commentRes.rows || [],
-        })
+        res
+            .json({
+                status: "ok",
+                board: boardRes.rows[0] || {},
+                posts: commentsRes.rows,
+            })
+            .end()
+        await client.end()
     })
-}
 
+    const PORT = process.env.PORT || 3000
+    app.use(express.static("./static"))
+    app.listen(PORT)
+
+    console.log(`running on http://localhost:${PORT}`)
+}
 init()
