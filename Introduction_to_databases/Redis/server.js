30a57f88321d0865861484866558f3e368725ffa
Precioussoul <habsoff@gmail.com>
2023-10-26 19:39:48 +0100

    feat: update server.js content

--- Introduction_to_databases/Redis/server.js
+++ Introduction_to_databases/Redis/server.js
@@ -4,6 +4,39 @@ const redis = require("redis")
 const client = redis.createClient()
     // redis incur, a function that performs incur operations on redis database
 const rIncr = promisify(client.incr).bind(client)
+const rGet = promisify(client.get).bind(client)
+const rSetex = promisify(client.setex).bind(client)
+
+function cache(key, ttl, slowFn) {
+    return async function cacheFn(...props) {
+        const cachesResponse = await rGet(key)
+        if (cachesResponse) {
+            return cachesResponse
+        }
+
+        const result = await slowFn(...props)
+        await rSetex(key, ttl, result)
+        return result
+    }
+}
+
+async function verySlowAndExpensivePostgresSQLQuery() {
+    // todo here, you would write your postgres query
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(new Date().toUTCString())
+        }, 10000)
+    })
+
+    return promise
+}
+
+const cacheFn = cache(
+    "expensive_call",
+    10,
+    verySlowAndExpensivePostgresSQLQuery
+)
 
 async function init() {
     const app = express()
@@ -17,6 +50,17 @@ async function init() {
             status: 200,
             views,
         })
+    })
+
+    app.get("/get", async(req, res) => {
+        const data = await cacheFn()
+
+        res
+            .json({
+                status: 200,
+                data,
+            })
+            .end()
     })
 
     const PORT = 3000
